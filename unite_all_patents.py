8399e5749977da11abdd8a9856f2b6ab93b44e32
Cucum <Cucumento@gmail.com>
2023-04-04 19:07:08 +0300

    just aniting and wqord counting

--- unite_all_patents.py
+++ unite_all_patents.py
@@ -2,19 +2,19 @@ import os
 from multiprocessing import freeze_support
 
 import pandas as pd
-import gensim
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 
+
+# Функция для чтения файла и преобразования его в словарь
+def read_file(filename):
+    df = pd.read_excel(filename)
+    return df.to_dict('records')
+
 def main():
     directory = 'patent_unloadings'
     all_files = os.listdir(directory)
-
-    # Функция для чтения файла и преобразования его в словарь
-    def read_file(filename):
-        df = pd.read_excel(filename)
-        return df.to_dict('records')
 
     # Чтение всех файлов с расширением xlsx и преобразование их в словари
     data = []
@@ -29,35 +29,33 @@ def main():
     stop_words = stopwords.words('english')
     tokenizer = RegexpTokenizer(r'\w+')
 
-    # Предварительная обработка данных
-    texts = []
-    for text in data:
-        text = text['Title'] + ' ' + text['Abstract'] + ' ' + text['Claims']
+    # Обработка текста
+    def process_text(text):
         tokens = tokenizer.tokenize(text.lower())
-        stopped_tokens = [token for token in tokens if token not in stop_words]
-        texts.append(stopped_tokens)
+        meaningful_tokens = [token for token in tokens if token not in stop_words and len(token) > 2]
+        return meaningful_tokens
 
-    # Создание словаря и корпуса для модели LDA
-    dictionary = gensim.corpora.Dictionary(texts)
-    corpus = [dictionary.doc2bow(text) for text in texts]
 
-    # Обучение модели LDA на корпусе
-    lda_model = gensim.models.LdaMulticore(corpus, num_topics=16, id2word=dictionary, passes=2, workers=2)
+    # Создание словаря для каждого патента
+    word_counts = []
+    for i, row in df.iterrows():
+        text = row['Title'] + ' ' + row['Abstract']
+        tokens = process_text(text)
+        unique_tokens = list(set(tokens))
+        word_count = {token: tokens.count(token) for token in unique_tokens}
+        word_count = {k: v for k, v in word_count.items() if v > 0}
+        sorted_word_count = {k: v for k, v in sorted(word_count.items(), key=lambda item: item[1], reverse=True)}
+        word_counts.append(sorted_word_count)
 
-    # Получение списка тем для каждого документа
-    topics = []
-    for text in texts:
-        bow = dictionary.doc2bow(text)
-        topic_weights = lda_model.get_document_topics(bow, minimum_probability=0.0)
-        topic = max(topic_weights, key=lambda x: x[1])[0]
-        topics.append(topic)
-
+    # Добавление столбца с количеством вхождений каждого слова
+    df = pd.DataFrame(data)
+    df['Word Counts'] = word_counts
 
     # Запись данных в файл "united_patents.xlsx" с форматированием
-    df = pd.DataFrame(data)
-    df['Topic'] = topics
+
     writer = pd.ExcelWriter('united_patents.xlsx', engine='xlsxwriter')
-    df.to_excel(writer, index=False)
+    df.to_excel(writer, sheet_name='Sheet1', index=False)
+
 
     # Получение объекта xlsxwriter и добавление форматирования
     workbook = writer.book
@@ -77,9 +75,7 @@ def main():
 
     worksheet.set_column(0, len(data[0]), 60)
     worksheet.set_default_row(100)
-    for i, topic in lda_model.print_topics(num_topics=16, num_words=10):
-        print(f"Topic {i}: {topic}")
-    writer.save()
+    writer.close()
 
 
 if __name__ == '__main__':

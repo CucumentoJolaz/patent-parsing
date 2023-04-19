b822069d1cf9f04d953088688efd0d7d1e39879b
Cucumento <Cucumento@gmail.com>
2023-04-19 10:21:03 +0300

    Code cleaned from unused functions

--- main.py
+++ main.py
@@ -57,18 +57,6 @@ def define_sector_by_chat(patent_text):
         message = data["message"]
     return message
 
-
-# Обработка текста
-def process_text(text):
-    # Загрузка стоп-слов и настройка токенизации
-
-    stop_words = stopwords.words('english')
-    tokenizer = RegexpTokenizer(r'\w+')
-    tokens = tokenizer.tokenize(text.lower())
-    meaningful_tokens = [token for token in tokens if token not in stop_words and len(token) > 2]
-    return meaningful_tokens
-
-
 # Функция для чтения файла и преобразования его в словарь
 def read_file(filename):
     df = pd.read_excel(filename, engine='openpyxl')
@@ -88,20 +76,6 @@ def get_dataframe_with_patents():
 
     # Создание словаря для каждого патента
     return pd.DataFrame(data)
-
-
-def count_words(df):
-    word_counts = []
-    nltk.download('stopwords')
-    for i, row in df.iterrows():
-        text = row['Title'] + ' ' + row['Abstract'] + '' + row['Claims']
-        tokens = process_text(text)
-        unique_tokens = list(set(tokens))
-        words = {token: tokens.count(token) for token in unique_tokens}
-        word_count = {k: v for k, v in words.items() if v > 0}
-        sorted_word_count = {k: v for k, v in sorted(word_count.items(), key=lambda item: item[1], reverse=True)}
-        word_counts.append(sorted_word_count)
-    return word_counts
 
 
 def write_to_excel(df, filename="patents.xlsx"):
@@ -153,53 +127,6 @@ def write_to_excel(df, filename="patents.xlsx"):
 
     # Закрываем writer
     writer.close()
-
-
-def calculate_sector_tokens(patent_tokens: dict) -> dict:
-    """
-    Calculate how many sector-specific tokens are present in a patent.
-    :param patent_tokens: a dictionary of tokens in the patent
-    :return: a dictionary with the count of sector-specific tokens in the patent
-    """
-
-    rik_tokens = ['compounds', 'formulation', 'formulations', 'compound', 'preparation',
-                  'preparations', 'peroxide']
-    tiv_tokens = ['pipe', 'tube', 'tubes', 'fiber', 'fibers', 'filament', 'filaments']
-    gy_tokens = ['packaging', 'film', 'films', 'bag', 'bags', 'pouch', 'pouches', 'blow']
-    jy_tokens = ['box', 'boxes', 'carton', 'cartons', 'container', 'containers']
-
-    tokens_sector_counter = {rik: 0, tiv: 0, gy: 0, jy: 0, nd: 0}
-
-    patent_tokens_copy = patent_tokens.copy()
-    for token in patent_tokens_copy.keys():
-        if token in rik_tokens:
-            tokens_sector_counter[rik] += patent_tokens.pop(token)
-        elif token in tiv_tokens:
-            tokens_sector_counter[tiv] += patent_tokens.pop(token)
-        elif token in gy_tokens:
-            tokens_sector_counter[gy] += patent_tokens.pop(token)
-        elif token in jy_tokens:
-            tokens_sector_counter[jy] += patent_tokens.pop(token)
-        else:
-            tokens_sector_counter[nd] += patent_tokens.pop(token)
-
-    return tokens_sector_counter
-
-
-def identify_sector(patent_tokens: dict) -> str:
-    """
-    Identify the most likely sector for a given patent.
-    :param patent_tokens: a dictionary of tokens in the patent
-    :return: the sector of the patent: 'rik', 'tiv', 'gy', 'jy', 'nd'
-    """
-    section_tokens = calculate_sector_tokens(patent_tokens)
-    section_tokens_no_nd = section_tokens.copy()
-    section_tokens_no_nd.pop(nd)
-    max_sector = max(section_tokens_no_nd, key=section_tokens.get)
-
-    if not any(section_tokens_no_nd.values()):
-        return nd
-    return max_sector
 
 
 def main():
